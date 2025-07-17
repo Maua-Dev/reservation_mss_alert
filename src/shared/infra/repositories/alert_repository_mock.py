f5f72858a92cba2e75e0ecba64e022e79b1397dd
Leonardo Luiz Seixas Iorio <lseixas.iorio@gmail.com>
2025-07-16 23:07:13 -0300

    added alert repo mock with one method, added respective test, added interface with respective test

---
+++ src/shared/infra/repositories/alert_repository_mock.py
@@ -0,0 +1,38 @@
+from src.shared.domain.repositories.alert_repository_interface import IAlertRepository
+from src.shared.domain.entities.alert import Alert
+from typing import List
+import uuid
+import time
+
+class AlertRepositoryMock(IAlertRepository):
+    
+    alerts = List[Alert]
+    
+    def __init__(self):
+        
+        self.alerts = [
+            Alert (
+                alert_id=str(uuid.uuid4()), 
+                title = "Aviso1", 
+                description = "quadra molhada por conta da chuva", 
+                start_date = int(time.time()), 
+                end_date =  int(time.time()) + 7200000, 
+                # severity = 2,
+                is_permanent=True
+            ),
+            Alert (
+                alert_id=str(uuid.uuid4()), 
+                title = "Aviso2", 
+                description = "quadra molhada por conta da chuva", 
+                start_date = int(time.time()), 
+                end_date =  int(time.time()) + 7200000, 
+                # severity = 2,
+                is_permanent=False
+            )
+        ]
+        
+    def create_alert(self, alert: Alert) -> Alert:
+        
+        self.alerts.append(alert)
+        
+        return Alert
---
+++ tests/shared/infra/repositories/test_alert_repository_mock.py
@@ -0,0 +1,27 @@
+from src.shared.infra.repositories.alert_repository_mock import AlertRepositoryMock
+from src.shared.domain.entities.alert import Alert
+import uuid
+import time
+
+class TestAlertRepositoryMock():
+    
+    def test_create_alert(self):
+        
+        repo = AlertRepositoryMock()
+        
+        id = str(uuid.uuid4())
+        
+        new_alert = Alert (
+                alert_id=id,
+                title = "AvisoTest", 
+                description = "quadra molhada por conta da chuva", 
+                start_date = int(time.time()), 
+                end_date =  int(time.time()) + 7200000, 
+                # severity = 2,
+                is_permanent=True
+            )
+        
+        repo.create_alert(new_alert)
+        
+        assert new_alert.alert_id == repo.alerts[-1].alert_id
+        assert new_alert.title == repo.alerts[-1].title
