29b71a11531eb470cd7107d3556ef4410ef22412
Leonardo Luiz Seixas Iorio <lseixas.iorio@gmail.com>
2025-07-16 22:07:41 -0300

    upcased alert class

--- src/shared/domain/entities/alert.py
+++ src/shared/domain/entities/alert.py
@@ -2,7 +2,7 @@ import abc
 import uuid
 from src.shared.helpers.errors.domain_errors import EntityError, EntityParameterOrderDatesError
 
-class alert(abc.ABC):
+class Alert(abc.ABC):
     alert_id: str
     title: str
     description: str
@@ -18,31 +18,31 @@ class alert(abc.ABC):
                  end_date: int, 
                  severity: int):
         
-        if not alert.validate_alert_id(alert_id):
+        if not Alert.validate_alert_id(alert_id):
             raise EntityError("alert_id")
         self.alert_id = alert_id
         
-        if not alert.validate_title(title): 
+        if not Alert.validate_title(title): 
             raise EntityError("title")
-        if not alert.validate_title_first_char(title):
+        if not Alert.validate_title_first_char(title):
             raise EntityError("title")
         self.title = title
         
-        if not alert.validate_description(description): 
+        if not Alert.validate_description(description): 
             raise EntityError("description")
         self.description = description
         
-        if not alert.validate_dates(start_date, end_date): 
+        if not Alert.validate_dates(start_date, end_date): 
             raise EntityError("dates")
         self.start_date = start_date
         self.end_date = end_date
         
-        if not alert.validate_order_dates(start_date, end_date):
+        if not Alert.validate_order_dates(start_date, end_date):
             raise EntityParameterOrderDatesError(start_date, end_date)
         self.start_date = start_date
         self.end_date = end_date
         
-        if not alert.validate_severity(severity):
+        if not Alert.validate_severity(severity):
             raise EntityError("severity")
         self.severity = severity
     
--- tests/shared/domain/entities/test_alert.py
+++ tests/shared/domain/entities/test_alert.py
@@ -1,4 +1,4 @@
-from src.shared.domain.entities.alert import alert
+from src.shared.domain.entities.alert import Alert
 from src.shared.helpers.errors.domain_errors import EntityError, EntityParameterOrderDatesError
 import pytest
 import time
@@ -6,36 +6,36 @@ import uuid
 
 class Test_Alert:
     def test_alert(self):
-        alert (alert_id=str(uuid.uuid4()), title = "Aviso", description = "quadra molhada por conta da chuva", start_date = int(time.time()), end_date =  int(time.time()) + 7200000, severity = 2)
+        Alert (alert_id=str(uuid.uuid4()), title = "Aviso", description = "quadra molhada por conta da chuva", start_date = int(time.time()), end_date =  int(time.time()) + 7200000, severity = 2)
     
     def test_alert_id_is_none(self):
         with pytest.raises(EntityError):
-            alert(alert_id=None, title = None, description = "quadra molhada por conta da chuva", start_date = int(time.time()), end_date =  int(time.time()) + 7200000, severity = 2)
+            Alert(alert_id=None, title = None, description = "quadra molhada por conta da chuva", start_date = int(time.time()), end_date =  int(time.time()) + 7200000, severity = 2)
     
     def test_alert_id_is_not_string(self):
         with pytest.raises(EntityError):
-            alert(alert_id=1, title = None, description = "quadra molhada por conta da chuva", start_date = int(time.time()), end_date =  int(time.time()) + 7200000, severity = 2)
+            Alert(alert_id=1, title = None, description = "quadra molhada por conta da chuva", start_date = int(time.time()), end_date =  int(time.time()) + 7200000, severity = 2)
     
     def test_alert_it_is_not_valid_uuid(self):
         with pytest.raises(EntityError):
-            alert(alert_id="123321adf-asdas2122", title = None, description = "quadra molhada por conta da chuva", start_date = int(time.time()), end_date =  int(time.time()) + 7200000, severity = 2)
+            Alert(alert_id="123321adf-asdas2122", title = None, description = "quadra molhada por conta da chuva", start_date = int(time.time()), end_date =  int(time.time()) + 7200000, severity = 2)
     
     def test_title_is_none(self):
         with pytest.raises(EntityError):
-            alert(alert_id=str(uuid.uuid4()), title = None, description = "quadra molhada por conta da chuva", start_date = int(time.time()), end_date =  int(time.time()) + 7200000, severity = 2)
+            Alert(alert_id=str(uuid.uuid4()), title = None, description = "quadra molhada por conta da chuva", start_date = int(time.time()), end_date =  int(time.time()) + 7200000, severity = 2)
     
     def test_title_is_not_str(self):
         with pytest.raises(EntityError):
-            alert(alert_id=str(uuid.uuid4()), title = 123, description = "quadra molhada por conta da chuva", start_date = int(time.time()), end_date =  int(time.time()) + 7200000, severity = 2)
+            Alert(alert_id=str(uuid.uuid4()), title = 123, description = "quadra molhada por conta da chuva", start_date = int(time.time()), end_date =  int(time.time()) + 7200000, severity = 2)
     
     def test_title_first_char_is_not_uppercase(self):
         with pytest.raises(EntityError):
-            alert(
+            Alert(
                 alert_id=str(uuid.uuid4()), title="aviso",description="quadra molhada por conta da chuva", start_date=int(time.time()), end_date=int(time.time()) + 7200000,severity=2)
     
     def test_description_is_none(self):
         with pytest.raises(EntityError):
-            alert(
+            Alert(
                 alert_id=str(uuid.uuid4()), 
                 title="Aviso",
                 description=None,
@@ -46,7 +46,7 @@ class Test_Alert:
     
     def test_description_is_not_str(self):
         with pytest.raises(EntityError):
-            alert(
+            Alert(
                 alert_id=str(uuid.uuid4()), 
                 title="Aviso",
                 description=123,
@@ -57,7 +57,7 @@ class Test_Alert:
     
     def test_dates_are_not_ints(self):
         with pytest.raises(EntityError):
-            alert(
+            Alert(
                 alert_id=str(uuid.uuid4()), 
                 title="Aviso",
                 description="quadra molhada",
@@ -69,7 +69,7 @@ class Test_Alert:
     def test_start_date_greater_than_end_date(self):
         with pytest.raises(EntityParameterOrderDatesError):
             now = int(time.time())
-            alert(
+            Alert(
                 alert_id=str(uuid.uuid4()), 
                 title="Aviso",
                 description="quadra molhada",
@@ -80,7 +80,7 @@ class Test_Alert:
     
     def test_severity_is_none(self):
         with pytest.raises(EntityError):
-            alert(
+            Alert(
                 alert_id=str(uuid.uuid4()), 
                 title="Aviso",
                 description="quadra molhada",
@@ -92,7 +92,7 @@ class Test_Alert:
     @pytest.mark.parametrize("severity", [0, 4, -1, 10])
     def test_severity_out_of_bounds(self, severity):
         with pytest.raises(EntityError):
-            alert(
+            Alert(
                 alert_id=str(uuid.uuid4()), 
                 title="Aviso",
                 description="quadra molhada",
