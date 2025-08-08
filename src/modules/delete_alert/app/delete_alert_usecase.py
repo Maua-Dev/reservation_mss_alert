a35811cd26910a20a4ec1fd94ab521d07a551448
Leonardo Luiz Seixas Iorio <lseixas.iorio@gmail.com>
2025-08-08 17:32:42 -0300

    added usecase with eventbridge logic

---
+++ src/modules/delete_alert/app/delete_alert_usecase.py
@@ -0,0 +1,26 @@
+
+from src.shared.domain.repositories.alert_repository_interface import IAlertRepository
+from src.shared.clients.event_bridge_client import EventBridgeClient
+
+class DeleteAlertUsecase():
+    
+    def __init__(self, repo: IAlertRepository):
+        
+        self.repo = repo
+        
+    def __call__(self, alert_id: str):
+        
+        rule_name = f"one-time-trigger-for-alert-{alert_id}"
+        
+        try:
+            
+            EventBridgeClient.delete_trigger(rule_name=rule_name)
+            
+        except:
+            
+            #TODO Melhorar esse erro, criar algo no errors como: EventBridgeDeleteTriggerErorr
+            raise Exception
+        
+        deletet_alert = self.repo.delete_alert(alert_id=alert_id)
+        
+        return deletet_alert
