14161c16d956931d34e4e6ec7754b681562412b5
Leonardo Luiz Seixas Iorio <lseixas.iorio@gmail.com>
2025-08-11 17:24:11 -0300

    finshed route, missing some tests but everything seems ok

--- src/modules/delete_alert/app/delete_alert_usecase.py
+++ src/modules/delete_alert/app/delete_alert_usecase.py
@@ -1,6 +1,7 @@
-
+from src.shared.environments import Environments
 from src.shared.domain.repositories.alert_repository_interface import IAlertRepository
 from src.shared.clients.event_bridge_client import EventBridgeClient
+from src.shared.helpers.errors.usecase_errors import ForbiddenAction
 
 class DeleteAlertUsecase():
     
@@ -8,18 +9,26 @@ class DeleteAlertUsecase():
         
         self.repo = repo
         
-    def __call__(self, alert_id: str):
+    def __call__(self, 
+                 alert_id: str,
+                #  user_role: str
+                 ):
+        
+        # if user_role != "ADMIN":
+        #     raise ForbiddenAction("user, only admin can create rules and alerts")
         
         rule_name = f"one-time-trigger-for-alert-{alert_id}"
         
-        try:
+        if not Environments.get_envs().stage.value == "TEST" :
             
-            EventBridgeClient.delete_trigger(rule_name=rule_name)
-            
-        except:
-            
-            #TODO Melhorar esse erro, criar algo no errors como: EventBridgeDeleteTriggerErorr
-            raise Exception
+            try:
+                
+                EventBridgeClient.delete_trigger(rule_name=rule_name)
+                
+            except:
+                
+                #TODO Melhorar esse erro, criar algo no errors como: EventBridgeDeleteTriggerErorr
+                raise Exception
         
         deletet_alert = self.repo.delete_alert(alert_id=alert_id)
         
---
+++ tests/modules/delete_alert/app/test_delete_alert_controller.py
@@ -0,0 +1,26 @@
+from src.modules.delete_alert.app.delete_alert_controller import DeleteAlertController
+from src.modules.delete_alert.app.delete_alert_usecase import DeleteAlertUsecase
+from src.shared.infra.repositories.alert_repository_mock import AlertRepositoryMock
+from src.shared.helpers.external_interfaces.http_models import HttpRequest
+
+class TestDeleteAlertController():
+    
+    def test_delete_alert_controller(self):
+        
+        repo = AlertRepositoryMock()
+        
+        usecase = DeleteAlertUsecase(repo=repo)
+        
+        controller = DeleteAlertController(usecase=usecase)
+        
+        id = repo.alerts[0].alert_id
+        
+        request = HttpRequest(
+            body= {
+                "alert_id": id
+            }
+        )
+        
+        alert = controller(
+            request=request
+        )
