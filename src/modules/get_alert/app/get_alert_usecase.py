7edbacbfee9aaa43722d7d4a9e3c9f6409de5963
veloy25 <veloy.devmaua@gmail.com>
2025-08-25 16:18:19 -0300

    Validation removed from get_alert_usecase

---
+++ src/modules/get_alert/app/get_alert_usecase.py
@@ -0,0 +1,19 @@
+<<<<<<< HEAD
+=======
+from src.shared.domain.entities.alert import Alert
+from src.shared.domain.repositories.alert_repository_interface import IAlertRepository
+from src.shared.helpers.errors.domain_errors import EntityError
+from src.shared.infra.external.observability.observability_aws import ObservabilityAWS
+
+
+class GetAlertUseCase:
+    def __init__(self, repo: IAlertRepository, observability: ObservabilityAWS):
+        self.repo = repo
+        self.observability = observability
+        
+    def __call__(self, alert_id: str) -> Alert:
+        self.observability.log_usecase_in()
+        alert = self.repo.get_alert(alert_id)
+        self.observability.log_usecase_out()
+        return alert
+>>>>>>> f0e2a40 (Validation removed from get_alert_usecase)
