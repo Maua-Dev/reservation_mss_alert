6ff3e2aa184b95249de77a26c3e0ed4f1a7488d2
lseixas <lseixas.iorio@gmail.com>
2025-09-15 22:07:39 -0300

    reverted to dynamo

--- src/shared/environments.py
+++ src/shared/environments.py
@@ -71,7 +71,7 @@ class Environments:
             return AlertRepositoryMock
         elif Environments.get_envs().stage in [STAGE.DEV, STAGE.HOMOLOG, STAGE.PROD]:
             from src.shared.infra.repositories.alert_repository_dynamo import AlertRepositoryDynamo
-            return AlertRepositoryMock
+            return AlertRepositoryDynamo
         else:
             raise Exception("No repository found for this stage")
 
