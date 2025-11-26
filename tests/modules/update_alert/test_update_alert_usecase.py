1332a7e3f6d659d5ae7b3ec3e09f82f0dc5bcb88
veloy25 <veloy.devmaua@gmail.com>
2025-11-26 18:14:18 -0300

    fix(update_alert_usecase): - Added new role parameter into the __call__ function - Added role, and dates validation - Added rollback method in case the new changes could not be done to the repository

--- tests/modules/update_alert/test_update_alert_usecase.py
+++ tests/modules/update_alert/test_update_alert_usecase.py
@@ -1,24 +1,24 @@
 import pytest
-from src.shared.domain.entities.alert import Alert
 from src.shared.infra.repositories.alert_repository_mock import AlertRepositoryMock
-from src.shared.helpers.errors.domain_errors import EntityError, EntityParameterOrderDatesError
 from src.modules.update_alert.app.update_alert_usecase import UpdateAlertUsecase
+from src.shared.helpers.errors.usecase_errors import ForbiddenAction 
 import time
 
 
-class Test_UpdateAlertUsecase:
-    def test_update_alert_usecase_update_title(self):
+class TestUpdateAlertUsecase:
+    def test_update_alert_usecase_user_is_admin(self):
         repo = AlertRepositoryMock()
         usecase = UpdateAlertUsecase(repo=repo)
 
         alert_id = repo.alerts[1].alert_id
 
         alert = usecase(
+            role="ADMIN",
             alert_id=alert_id, 
             new_title="New Title", 
             new_description="cuidado",
-            new_start_date=time.time(),
-            new_end_date=time.time()+3600,
+            new_start_date=int(time.time()),
+            new_end_date=int(time.time()+3600),
             new_is_rule=False
         )
 
@@ -29,4 +29,21 @@ class Test_UpdateAlertUsecase:
         assert abs(alert.end_date - (time.time()+3600)) < 2
         assert alert.is_rule is False
 
-        
+    def test_update_alert_usecase_user_not_admin(self):
+        repo = AlertRepositoryMock()
+        usecase = UpdateAlertUsecase(repo=repo)
+
+        alert_id = repo.alerts[1].alert_id
+
+        with pytest.raises(ForbiddenAction) as error_info:
+            usecase(
+                role="notADMIN",
+                alert_id=alert_id, 
+                new_title="New Title", 
+                new_description="cuidado",
+                new_start_date=int(time.time()),
+                new_end_date=int(time.time()+3600),
+                new_is_rule=False
+            )
+    
+        assert str(error_info.value) == 'That action is forbidden for this role'
