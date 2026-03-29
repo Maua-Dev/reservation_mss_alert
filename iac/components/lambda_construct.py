ea6132905b83bea2e8112ae96362c1003a5c7b74
Leo Iorio <lseixas.iorio@gmail.com>
2026-03-29 15:21:09 -0300

    fixing layer folder

--- iac/components/lambda_construct.py
+++ iac/components/lambda_construct.py
@@ -59,7 +59,7 @@ class LambdaConstruct(Construct):
         self.lambda_layer = lambda_.LayerVersion(
             self, 
             id="ReservationMssAlert_Layer",
-            code=lambda_.Code.from_asset("./lambda_layer_out_temp"),
+            code=lambda_.Code.from_asset("./build"),
             compatible_runtimes=[lambda_.Runtime("python3.13")]
         )
 
